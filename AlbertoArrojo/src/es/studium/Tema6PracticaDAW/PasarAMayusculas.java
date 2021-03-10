66f7aa24712ec780515d3443787def18fb3ac424
Alberto <57352487+AlbertoArrojoC@users.noreply.github.com>
2021-03-10 11:55:21 +0100

    Update PasarAMayusculas.java

--- AlbertoArrojo/src/es/studium/Tema6PracticaDAW/PasarAMayusculas.java
+++ AlbertoArrojo/src/es/studium/Tema6PracticaDAW/PasarAMayusculas.java
@@ -28,9 +28,9 @@ public class PasarAMayusculas extends HttpServlet {
 	out.println("<head><title>Tema 6 DAW - Alberto Arrojo Carrasco</title>  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl\' crossorigin='anonymous'></head>");
 	out.println("<body>");
 	out.println(
-		"<p class='lead text-center mt-5'>La palabra que escribiste se ha pasado al Servlet por el método POST y el resultado es: <strong>" + funcionMayusculas(texto) + "</strong><br></p>");
+		"<p class='lead text-center mt-5'>La palabra que escribiste se ha pasado al Servlet por el mÃ©todo POST y el resultado es: <strong>" + funcionMayusculas(texto) + "</strong><br></p>");
 	out.println("<div class='text-center'>");
-	out.println("<a href='/AlbertoArrojo' class='btn btn-primary'>Volver</a>");
+	out.println("<a href='./index.html' class='btn btn-primary'>Volver</a>");
 	out.println("</div>");
 	out.println("</body></html>");
 	
